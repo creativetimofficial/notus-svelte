bd42c5854aeff476cbfbfc8e284737d19707e940
EINazare <emanuelioannazare@gmail.com>
2020-09-09 16:34:28 +0300

    refactor: add Index from Tailwind Webapp on / route

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,23 @@
 <!-- App.svelte -->
 <script>
   import { Router, Link, Route } from "svelte-routing";
-  import Home from "./routes/Home.svelte";
-  import About from "./routes/About.svelte";
-  import Blog from "./routes/Blog.svelte";
+  import Index from "./routes/Index.svelte";
+  // import About from "./routes/About.svelte";
+  // import Blog from "./routes/Blog.svelte";
 
   export let url = "";
 </script>
 
+<link rel="stylesheet" href="../node-modules/@fortawesome/fontawesome-free/css/all.min.css">
+<link rel="stylesheet" href="./assets/styles/tailwind.css">
+
 <Router url="{url}">
-  <nav>
+  <!-- <nav>
     <Link to="/">Home</Link>
     <Link to="about">About</Link>
     <Link to="blog">Blog</Link>
-  </nav>
-  <div>
-    <Route path="blog/*" component="{Blog}" />
-    <Route path="about" component="{About}" />
-    <Route path="/"><Home /></Route>
-  </div>
+  </nav> -->
+    <!-- <Route path="blog/*" component="{Blog}" />
+    <Route path="about" component="{About}" /> -->
+  <Route path="/" component="{Index}" />
 </Router>
