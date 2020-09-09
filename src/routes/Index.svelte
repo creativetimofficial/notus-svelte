17f7a1fc2693853758a1fd348d950b50549770bc
EINazare <emanuelioannazare@gmail.com>
2020-09-09 17:38:53 +0300

    refactor: move assets to public folder

--- src/routes/Index.svelte
+++ src/routes/Index.svelte
@@ -2,17 +2,17 @@
 import IndexNavbar from "components/Navbars/IndexNavbar.svelte";
 import Footer from "components/Footers/Footer.svelte";
 
-const patternVue = "../assets/img/pattern_vue.png";
-const componentBtn = "../assets/img/component-btn.png";
-const componentProfileCard = "../assets/img/component-profile-card.png";
-const componentInfoCard = "../assets/img/component-info-card.png";
-const componentInfo2 = "../assets/img/component-info-2.png";
-const componentMenu = "../assets/img/component-menu.png";
-const componentBtnPink = "../assets/img/component-btn-pink.png";
-const documentation = "../assets/img/documentation.png";
-const login = "../assets/img/login.jpg";
-const profile = "../assets/img/profile.jpg";
-const landing = "../assets/img/landing.jpg";
+const patternVue = "/assets/img/pattern_vue.png";
+const componentBtn = "/assets/img/component-btn.png";
+const componentProfileCard = "/assets/img/component-profile-card.png";
+const componentInfoCard = "/assets/img/component-info-card.png";
+const componentInfo2 = "/assets/img/component-info-2.png";
+const componentMenu = "/assets/img/component-menu.png";
+const componentBtnPink = "/assets/img/component-btn-pink.png";
+const documentation = "/assets/img/documentation.png";
+const login = "/assets/img/login.jpg";
+const profile = "/assets/img/profile.jpg";
+const landing = "/assets/img/landing.jpg";
 
 </script>
 
