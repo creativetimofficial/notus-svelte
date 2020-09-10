6741126fdc7ad3dc8fc4938947afe1bec5ad9036
EINazare <emanuelioannazare@gmail.com>
2020-09-10 18:21:49 +0300

    feature: add file for Maps

---
+++ src/views/admin/Maps.svelte
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-wrap">
+    <div class="w-full px-4">
+      <div
+        class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded"
+      >
+        <map-example />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import MapExample from "@/components/Maps/MapExample.vue";
+
+export default {
+  components: {
+    MapExample,
+  },
+};
+</script>
