32ba4da2dcebfd84da6c62f15bc6a6afd5266906
EINazare <emanuelioannazare@gmail.com>
2020-09-10 20:44:13 +0300

    style: run prettier

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
 const app = new App({
-	target: document.getElementById("app"),
-	props: {
-		name: 'world'
-	}
+  target: document.getElementById("app"),
+  props: {
+    name: "world",
+  },
 });
 
 export default app;
