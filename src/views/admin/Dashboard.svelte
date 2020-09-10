49389f65f3ba5fb1f6b1af530625c6bf87800297
EINazare <emanuelioannazare@gmail.com>
2020-09-10 18:21:45 +0300

    feature: add file for Dashboard

---
+++ src/views/admin/Dashboard.svelte
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="flex flex-wrap">
+      <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+        <card-line-chart />
+      </div>
+      <div class="w-full xl:w-4/12 px-4">
+        <card-bar-chart />
+      </div>
+    </div>
+    <div class="flex flex-wrap mt-4">
+      <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+        <card-page-visits />
+      </div>
+      <div class="w-full xl:w-4/12 px-4">
+        <card-social-traffic />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import CardLineChart from "@/components/Cards/CardLineChart.vue";
+import CardBarChart from "@/components/Cards/CardBarChart.vue";
+import CardPageVisits from "@/components/Cards/CardPageVisits.vue";
+import CardSocialTraffic from "@/components/Cards/CardSocialTraffic.vue";
+export default {
+  name: "dashboard-page",
+  components: {
+    CardLineChart,
+    CardBarChart,
+    CardPageVisits,
+    CardSocialTraffic,
+  },
+};
+</script>
