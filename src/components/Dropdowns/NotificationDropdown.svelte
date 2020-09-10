1d9a9bf469b9c7bbdfb3db2c75a646b12b3e7156
EINazare <emanuelioannazare@gmail.com>
2020-09-10 18:18:53 +0300

    feature: add file for NotificationDropdown

---
+++ src/components/Dropdowns/NotificationDropdown.svelte
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <a
+      class="text-gray-600 block py-1 px-3"
+      ref="btnDropdownRef"
+      v-on:click="toggleDropdown($event)"
+    >
+      <i class="fas fa-bell"></i>
+    </a>
+    <div
+      ref="popoverDropdownRef"
+      class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
+      v-bind:class="{
+        hidden: !dropdownPopoverShow,
+        block: dropdownPopoverShow,
+      }"
+    >
+      <a
+        href="javascript:void(0);"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Action
+      </a>
+      <a
+        href="javascript:void(0);"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Another action
+      </a>
+      <a
+        href="javascript:void(0);"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Something else here
+      </a>
+      <div class="h-0 my-2 border border-solid border-gray-200" />
+      <a
+        href="javascript:void(0);"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Seprated link
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+import { createPopper } from "@popperjs/core";
+
+export default {
+  data() {
+    return {
+      dropdownPopoverShow: false,
+    };
+  },
+  methods: {
+    toggleDropdown: function (event) {
+      event.preventDefault();
+      if (this.dropdownPopoverShow) {
+        this.dropdownPopoverShow = false;
+      } else {
+        this.dropdownPopoverShow = true;
+        createPopper(this.$refs.btnDropdownRef, this.$refs.popoverDropdownRef, {
+          placement: "bottom-start",
+        });
+      }
+    },
+  },
+};
+</script>
