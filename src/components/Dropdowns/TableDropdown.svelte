06e8c8dea451d2fd625893f482bfa682ac43d485
EINazare <emanuelioannazare@gmail.com>
2020-09-10 18:19:06 +0300

    feature: add file for TablesDropdown

---
+++ src/components/Dropdowns/TableDropdown.svelte
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <a
+      class="text-gray-600 py-1 px-3"
+      href="#pablo"
+      ref="btnDropdownRef"
+      v-on:click="toggleDropdown($event)"
+    >
+      <i class="fas fa-ellipsis-v"></i>
+    </a>
+    <div
+      ref="popoverDropdownRef"
+      class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
+      v-bind:class="{
+        hidden: !dropdownPopoverShow,
+        block: dropdownPopoverShow,
+      }"
+    >
+      <a
+        href="javascript:void(0);"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Action
+      </a>
+      <a
+        href="javascript:void(0);"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Another action
+      </a>
+      <a
+        href="javascript:void(0);"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Something else here
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+import { createPopper } from "@popperjs/core";
+
+export default {
+  data() {
+    return {
+      dropdownPopoverShow: false,
+    };
+  },
+  methods: {
+    toggleDropdown: function (event) {
+      event.preventDefault();
+      if (this.dropdownPopoverShow) {
+        this.dropdownPopoverShow = false;
+      } else {
+        this.dropdownPopoverShow = true;
+        createPopper(this.$refs.btnDropdownRef, this.$refs.popoverDropdownRef, {
+          placement: "bottom-start",
+        });
+      }
+    },
+  },
+};
+</script>
