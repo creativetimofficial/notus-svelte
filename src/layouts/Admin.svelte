271dc3ec7ae8c9bcb9d850805f6a1f019ecde0d9
EINazare <emanuelioannazare@gmail.com>
2020-09-10 18:21:27 +0300

    feature: add file for Admin

---
+++ src/layouts/Admin.svelte
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <sidebar />
+    <div class="relative md:ml-64 bg-gray-200">
+      <admin-navbar />
+      <header-stats />
+      <div class="px-4 md:px-10 mx-auto w-full -m-24">
+        <router-view />
+        <footer-admin />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import AdminNavbar from "@/components/Navbars/AdminNavbar.vue";
+import Sidebar from "@/components/Sidebar/Sidebar.vue";
+import HeaderStats from "@/components/Headers/HeaderStats.vue";
+import FooterAdmin from "@/components/Footers/FooterAdmin.vue";
+export default {
+  name: "admin-layout",
+  components: {
+    AdminNavbar,
+    Sidebar,
+    HeaderStats,
+    FooterAdmin,
+  },
+};
+</script>
