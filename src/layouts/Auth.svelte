ddcd91b86b90705d5a77633f51b24f4729a4e341
EINazare <emanuelioannazare@gmail.com>
2020-09-10 20:15:54 +0300

    feature: add Auth Svelte code

--- src/layouts/Auth.svelte
+++ src/layouts/Auth.svelte
@@ -1,33 +1,31 @@
-<template>
+<script>
+import { Router, Route } from "svelte-routing";
+
+// components for this layout
+import AuthNavbar from "components/Navbars/AuthNavbar.svelte";
+import FooterSmall from "components/Footers/FooterSmall.svelte";
+
+// pages for this layout
+import Login from "views/auth/Login.svelte";
+import Register from "views/auth/Register.svelte";
+
+const registerBg2 = "../assets/img/register_bg_2.png";
+</script>
+
+
   <div>
-    <navbar />
+    <AuthNavbar />
     <main>
       <section class="relative w-full h-full py-40">
         <div
           class="absolute top-0 w-full h-full bg-gray-900 bg-no-repeat bg-full"
-          :style="`background-image: url('${registerBg2}');`"
+          style="background-image: url({registerBg2});"
         ></div>
-        <router-view />
-        <footer-small absolute />
+        <Router url="auth">
+          <Route path="login" component="{Login}" />
+          <Route path="register" component="{Register}" />
+        </Router>
+        <FooterSmall absolute="true" />
       </section>
     </main>
   </div>
-</template>
-<script>
-import Navbar from "@/components/Navbars/AuthNavbar.vue";
-import FooterSmall from "@/components/Footers/FooterSmall.vue";
-
-import registerBg2 from "@/assets/img/register_bg_2.png";
-
-export default {
-  data() {
-    return {
-      registerBg2,
-    };
-  },
-  components: {
-    Navbar,
-    FooterSmall,
-  },
-};
-</script>
