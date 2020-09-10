6476325064355ae71a79946a7582a9a65f7e57d4
EINazare <emanuelioannazare@gmail.com>
2020-09-10 18:21:56 +0300

    feature: add file for Settings

---
+++ src/views/admin/Settings.svelte
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-wrap">
+    <div class="w-full lg:w-8/12 px-4">
+      <CardSettings />
+    </div>
+    <div class="w-full lg:w-4/12 px-4">
+      <CardProfile />
+    </div>
+  </div>
+</template>
+<script>
+import CardSettings from "@/components/Cards/CardSettings.vue";
+import CardProfile from "@/components/Cards/CardProfile.vue";
+
+export default {
+  components: {
+    CardSettings,
+    CardProfile,
+  },
+};
+</script>
