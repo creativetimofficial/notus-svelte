e0eacb03ff2a5244ae0938a8bc939e00582b1682
EINazare <emanuelioannazare@gmail.com>
2020-09-10 20:40:43 +0300

    feature: add Admin Svelte code

--- src/layouts/Admin.svelte
+++ src/layouts/Admin.svelte
@@ -1,28 +1,34 @@
-<template>
+<script>
+import { Router, Route } from "svelte-routing";
+
+// components for this layout
+import AdminNavbar from "components/Navbars/AdminNavbar.svelte";
+import Sidebar from "components/Sidebar/Sidebar.svelte";
+import HeaderStats from "components/Headers/HeaderStats.svelte";
+import FooterAdmin from "components/Footers/FooterAdmin.svelte";
+
+// pages for this layout
+// import Dashboard from "views/admin/Dashboard.svelte";
+// import Settings from "views/admin/Settings.svelte";
+// import Tables from "views/admin/Tables.svelte";
+// import Maps from "views/admin/Maps.svelte";
+
+</script>
+
+
   <div>
-    <sidebar />
+    <Sidebar />
     <div class="relative md:ml-64 bg-gray-200">
-      <admin-navbar />
-      <header-stats />
+      <AdminNavbar />
+      <HeaderStats />
       <div class="px-4 md:px-10 mx-auto w-full -m-24">
-        <router-view />
-        <footer-admin />
+        <!-- <Router url="admin">
+          <Route path="dashboard" component="{Dashboard}" />
+          <Route path="settings" component="{Settings}" />
+          <Route path="tables" component="{Tables}" />
+          <Route path="maps" component="{Maps}" />
+        </Router> -->
+        <FooterAdmin />
       </div>
     </div>
   </div>
-</template>
-<script>
-import AdminNavbar from "@/components/Navbars/AdminNavbar.vue";
-import Sidebar from "@/components/Sidebar/Sidebar.vue";
-import HeaderStats from "@/components/Headers/HeaderStats.vue";
-import FooterAdmin from "@/components/Footers/FooterAdmin.vue";
-export default {
-  name: "admin-layout",
-  components: {
-    AdminNavbar,
-    Sidebar,
-    HeaderStats,
-    FooterAdmin,
-  },
-};
-</script>
