e804c08ec54f2d739c89ec0be05c6af985251a89
EINazare <emanuelioannazare@gmail.com>
2020-09-10 20:40:19 +0300

    feature: add UserDropdown Svelte code

--- src/components/Dropdowns/UserDropdown.svelte
+++ src/components/Dropdowns/UserDropdown.svelte
@@ -1,10 +1,34 @@
-<template>
+<script>
+import { createPopper } from "@popperjs/core";
+
+const image = "../assets/img/team-1-800x800.jpg";
+
+let dropdownPopoverShow = false;
+
+let btnDropdownRef;
+let popoverDropdownRef;
+
+const toggleDropdown = (event) => {
+  event.preventDefault();
+  if (dropdownPopoverShow) {
+    dropdownPopoverShow = false;
+  } else {
+    dropdownPopoverShow = true;
+    createPopper(btnDropdownRef, popoverDropdownRef, {
+      placement: "bottom-start",
+    });
+  }
+}
+</script>
+
+
+
   <div>
     <a
       class="text-gray-600 block"
       href="#pablo"
-      ref="btnDropdownRef"
-      v-on:click="toggleDropdown($event)"
+      bind:this={btnDropdownRef}
+      on:click="{toggleDropdown}"
     >
       <div class="items-center flex">
         <span
@@ -13,18 +37,14 @@
           <img
             alt="..."
             class="w-full rounded-full align-middle border-none shadow-lg"
-            :src="image"
+            src={image}
           />
         </span>
       </div>
     </a>
     <div
-      ref="popoverDropdownRef"
-      class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
-      v-bind:class="{
-        hidden: !dropdownPopoverShow,
-        block: dropdownPopoverShow,
-      }"
+      bind:this={popoverDropdownRef}
+      class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48 {dropdownPopoverShow ? 'block':'hidden'}"
     >
       <a
         href="javascript:void(0);"
@@ -53,32 +73,3 @@
       </a>
     </div>
   </div>
-</template>
-
-<script>
-import { createPopper } from "@popperjs/core";
-
-import image from "@/assets/img/team-1-800x800.jpg";
-
-export default {
-  data() {
-    return {
-      dropdownPopoverShow: false,
-      image: image,
-    };
-  },
-  methods: {
-    toggleDropdown: function (event) {
-      event.preventDefault();
-      if (this.dropdownPopoverShow) {
-        this.dropdownPopoverShow = false;
-      } else {
-        this.dropdownPopoverShow = true;
-        createPopper(this.$refs.btnDropdownRef, this.$refs.popoverDropdownRef, {
-          placement: "bottom-start",
-        });
-      }
-    },
-  },
-};
-</script>
