7e74f9a530bd3b10446bf3531dd1848d054cc866
EINazare <emanuelioannazare@gmail.com>
2020-09-11 19:21:16 +0300

    refactor: add metatags and scripts

--- rollup.config.js
+++ rollup.config.js
@@ -20,6 +20,92 @@ const aliases = alias({
 const indexTemplate = `<!DOCTYPE html>
 <html lang="en">
   <head>
+    <!-- Anti-flicker snippet (recommended)  -->
+    <style>.async-hide { opacity: 0 !important} </style>
+    <script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+    h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+    (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+    })(window,document.documentElement,'async-hide','dataLayer',4000,
+    {'GTM-K9BGS8K':true});</script>
+
+    <!-- Analytics-Optimize Snippet -->
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-46172202-22', 'auto', {allowLinker: true});
+    ga('set', 'anonymizeIp', true);
+    ga('require', 'GTM-K9BGS8K');
+    ga('require', 'displayfeatures');
+    ga('require', 'linker');
+    ga('linker:autoLink', ["2checkout.com","avangate.com"]);
+    </script>
+    <!-- end Analytics-Optimize Snippet -->
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NKDMSK6');</script>
+    <!-- End Google Tag Manager -->
+    <!-- Extra details for Live View on GitHub Pages -->
+    <!-- Canonical SEO -->
+    <link
+      rel="canonical"
+      href="https://www.creative-tim.com/product/tailwind-webapp-svelte"
+    />
+
+    <!--  Social tags      -->
+    <meta
+      name="keywords"
+      content="free template, free kit, free dashboard, free webapp, free web app, freebie, free, kit, dashboard, webapp, we bapp, starter kit, starter dashboard, starter admin, starter webapp, starter web app, tailwind, tailwindcss, html kit, svelte kit, html dashboard, svelte dashboard, html webapp, svelte webapp html web app, svelte web app"
+    />
+    <meta
+      name="description"
+      content="Start your development with a Free Tailwindcss and Svelte Admin and Presentation Starter Kit Template. Let Tailwind Webapp Svelte amaze you with its cool features and build tools and get your project to a whole new level. "
+    />
+
+    <!-- Schema.org markup for Google+ -->
+    <meta itemprop="name" content="Tailwind Webapp Svelte by Creative Tim" />
+    <meta
+      itemprop="description"
+      content="Start your development with a Free Tailwindcss and Svelte Admin and Presentation Starter Kit Template. Let Tailwind Webapp Svelte amaze you with its cool features and build tools and get your project to a whole new level. "
+    />
+
+    <meta itemprop="image" content="<<product_image>>" />
+
+    <!-- Twitter Card data -->
+    <meta name="twitter:card" content="product" />
+    <meta name="twitter:site" content="@creativetim" />
+    <meta
+      name="twitter:title"
+      content="Tailwind Webapp Svelte by Creative Tim"
+    />
+
+    <meta
+      name="twitter:description"
+      content="Start your development with a Free Tailwindcss and Svelte Admin and Presentation Starter Kit Template. Let Tailwind Webapp Svelte amaze you with its cool features and build tools and get your project to a whole new level. "
+    />
+    <meta name="twitter:creator" content="@creativetim" />
+    <meta name="twitter:image" content="<<product_image>>" />
+
+    <!-- Open Graph data -->
+    <meta property="fb:app_id" content="655968634437471" />
+    <meta property="og:title" content="Tailwind Webapp Svelte by Creative Tim" />
+    <meta property="og:type" content="article" />
+    <meta
+      property="og:url"
+      content="http://demos.creative-tim.com/tailwind-webapp-svelte/#/"
+    />
+    <meta property="og:image" content="<<product_image>>" />
+    <meta
+      property="og:description"
+      content="Start your development with a Free Tailwindcss and Svelte Admin and Presentation Starter Kit Template. Let Tailwind Webapp Svelte amaze you with its cool features and build tools and get your project to a whole new level. "
+    />
+    <meta property="og:site_name" content="Creative Tim" />
+
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
 
@@ -43,6 +129,10 @@ const indexTemplate = `<!DOCTYPE html>
   </head>
 
   <body class="text-gray-800 antialiased">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKDMSK6"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <noscript>
       <strong
         >We're sorry but tailwind-webapp-svelte doesn't work properly without
