94e2ecc4780210d4a8bc0b458de4bd19dc724871
EINazare <emanuelioannazare@gmail.com>
2020-09-11 10:13:47 +0300

    feature: add CardBarChart Svelte code

--- src/components/Cards/CardBarChart.svelte
+++ src/components/Cards/CardBarChart.svelte
@@ -1,4 +1,107 @@
-<template>
+<script>
+import { onMount } from 'svelte';
+import Chart from "chart.js";
+
+onMount(async () => {
+  let config = {
+    type: "bar",
+    data: {
+      labels: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+      ],
+      datasets: [
+        {
+          label: new Date().getFullYear(),
+          backgroundColor: "#ed64a6",
+          borderColor: "#ed64a6",
+          data: [30, 78, 56, 34, 100, 45, 13],
+          fill: false,
+          barThickness: 8,
+        },
+        {
+          label: new Date().getFullYear() - 1,
+          fill: false,
+          backgroundColor: "#38a169",
+          borderColor: "#38a169",
+          data: [27, 68, 86, 74, 10, 4, 87],
+          barThickness: 8,
+        },
+      ],
+    },
+    options: {
+      maintainAspectRatio: false,
+      responsive: true,
+      title: {
+        display: false,
+        text: "Orders Chart",
+      },
+      tooltips: {
+        mode: "index",
+        intersect: false,
+      },
+      hover: {
+        mode: "nearest",
+        intersect: true,
+      },
+      legend: {
+        labels: {
+          fontColor: "rgba(0,0,0,.4)",
+        },
+        align: "end",
+        position: "bottom",
+      },
+      scales: {
+        xAxes: [
+          {
+            display: false,
+            scaleLabel: {
+              display: true,
+              labelString: "Month",
+            },
+            gridLines: {
+              borderDash: [2],
+              borderDashOffset: [2],
+              color: "rgba(33, 37, 41, 0.3)",
+              zeroLineColor: "rgba(33, 37, 41, 0.3)",
+              zeroLineBorderDash: [2],
+              zeroLineBorderDashOffset: [2],
+            },
+          },
+        ],
+        yAxes: [
+          {
+            display: true,
+            scaleLabel: {
+              display: false,
+              labelString: "Value",
+            },
+            gridLines: {
+              borderDash: [2],
+              drawBorder: false,
+              borderDashOffset: [2],
+              color: "rgba(33, 37, 41, 0.2)",
+              zeroLineColor: "rgba(33, 37, 41, 0.15)",
+              zeroLineBorderDash: [2],
+              zeroLineBorderDashOffset: [2],
+            },
+          },
+        ],
+      },
+    },
+  };
+  let ctx = document.getElementById("bar-chart").getContext("2d");
+  window.myBar = new Chart(ctx, config);
+});
+</script>
+
+
+
   <div
     class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded"
   >
@@ -20,107 +123,3 @@
       </div>
     </div>
   </div>
-</template>
-<script>
-import Chart from "chart.js";
-export default {
-  mounted: function () {
-    this.$nextTick(function () {
-      let config = {
-        type: "bar",
-        data: {
-          labels: [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-          ],
-          datasets: [
-            {
-              label: new Date().getFullYear(),
-              backgroundColor: "#ed64a6",
-              borderColor: "#ed64a6",
-              data: [30, 78, 56, 34, 100, 45, 13],
-              fill: false,
-              barThickness: 8,
-            },
-            {
-              label: new Date().getFullYear() - 1,
-              fill: false,
-              backgroundColor: "#38a169",
-              borderColor: "#38a169",
-              data: [27, 68, 86, 74, 10, 4, 87],
-              barThickness: 8,
-            },
-          ],
-        },
-        options: {
-          maintainAspectRatio: false,
-          responsive: true,
-          title: {
-            display: false,
-            text: "Orders Chart",
-          },
-          tooltips: {
-            mode: "index",
-            intersect: false,
-          },
-          hover: {
-            mode: "nearest",
-            intersect: true,
-          },
-          legend: {
-            labels: {
-              fontColor: "rgba(0,0,0,.4)",
-            },
-            align: "end",
-            position: "bottom",
-          },
-          scales: {
-            xAxes: [
-              {
-                display: false,
-                scaleLabel: {
-                  display: true,
-                  labelString: "Month",
-                },
-                gridLines: {
-                  borderDash: [2],
-                  borderDashOffset: [2],
-                  color: "rgba(33, 37, 41, 0.3)",
-                  zeroLineColor: "rgba(33, 37, 41, 0.3)",
-                  zeroLineBorderDash: [2],
-                  zeroLineBorderDashOffset: [2],
-                },
-              },
-            ],
-            yAxes: [
-              {
-                display: true,
-                scaleLabel: {
-                  display: false,
-                  labelString: "Value",
-                },
-                gridLines: {
-                  borderDash: [2],
-                  drawBorder: false,
-                  borderDashOffset: [2],
-                  color: "rgba(33, 37, 41, 0.2)",
-                  zeroLineColor: "rgba(33, 37, 41, 0.15)",
-                  zeroLineBorderDash: [2],
-                  zeroLineBorderDashOffset: [2],
-                },
-              },
-            ],
-          },
-        },
-      };
-      let ctx = document.getElementById("bar-chart").getContext("2d");
-      window.myBar = new Chart(ctx, config);
-    });
-  },
-};
-</script>
