e254786942f9336bee9fc458a7057470202c64ea
EINazare <emanuelioannazare@gmail.com>
2020-09-11 10:14:55 +0300

    feature: add Admin Svelte code

--- src/layouts/Admin.svelte
+++ src/layouts/Admin.svelte
@@ -8,10 +8,10 @@ import HeaderStats from "components/Headers/HeaderStats.svelte";
 import FooterAdmin from "components/Footers/FooterAdmin.svelte";
 
 // pages for this layout
-// import Dashboard from "views/admin/Dashboard.svelte";
-// import Settings from "views/admin/Settings.svelte";
-// import Tables from "views/admin/Tables.svelte";
-// import Maps from "views/admin/Maps.svelte";
+import Dashboard from "views/admin/Dashboard.svelte";
+import Settings from "views/admin/Settings.svelte";
+import Tables from "views/admin/Tables.svelte";
+import Maps from "views/admin/Maps.svelte";
 
 </script>
 
@@ -22,12 +22,12 @@ import FooterAdmin from "components/Footers/FooterAdmin.svelte";
       <AdminNavbar />
       <HeaderStats />
       <div class="px-4 md:px-10 mx-auto w-full -m-24">
-        <!-- <Router url="admin">
+        <Router url="admin">
           <Route path="dashboard" component="{Dashboard}" />
           <Route path="settings" component="{Settings}" />
           <Route path="tables" component="{Tables}" />
           <Route path="maps" component="{Maps}" />
-        </Router> -->
+        </Router>
         <FooterAdmin />
       </div>
     </div>
