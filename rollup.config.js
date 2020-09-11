22a464d61cb2e5e622a5d578267f441b7355a516
EINazare <emanuelioannazare@gmail.com>
2020-09-11 18:52:12 +0300

    refactor: try to solve github pages live preview

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import alias from "@rollup/plugin-alias";
+import fs from "fs";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -15,6 +16,53 @@ const aliases = alias({
     { find: "assets", replacement: "src/assets" },
   ],
 });
+
+const indexTemplate = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+
+    <link rel="shortcut icon" href="<<live-preview-link>>/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="76x76" href="<<live-preview-link>>/apple-icon.png" />
+    <link rel="stylesheet" href="<<live-preview-link>>/build/bundle.css" />
+    <link
+      rel="stylesheet"
+      href="<<live-preview-link>>/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css"
+    />
+    <link rel="stylesheet" href="<<live-preview-link>>/assets/styles/tailwind.css" />
+    <title>Tailwind WebApp Svelte</title>
+
+    <script>
+      if (process === undefined) {
+        var process = { env: {<<process-env-status>>} };
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"></script>
+    <script defer src="<<live-preview-link>>/build/bundle.js"></script>
+  </head>
+
+  <body class="text-gray-800 antialiased">
+    <noscript>
+      <strong
+        >We're sorry but tailwind-webapp-svelte doesn't work properly without
+        JavaScript enabled. Please enable it to continue.</strong
+      >
+    </noscript>
+    <div id="app"></div>
+  </body>
+</html>
+`
+
+if (production) {
+  fs.writeFileSync("./public/index.html",indexTemplate.replace("<<process-env-status>>","PRODUCTION: true").replace(/<<live-preview-link>>/g,"/tailwind-webapp-svelte"));
+  fs.writeFileSync("./public/200.html",indexTemplate.replace("<<process-env-status>>","PRODUCTION: true").replace(/<<live-preview-link>>/g,"/tailwind-webapp-svelte"));
+  fs.writeFileSync("./public/404.html",indexTemplate.replace("<<process-env-status>>","PRODUCTION: true").replace(/<<live-preview-link>>/g,"/tailwind-webapp-svelte"));
+} else {
+  fs.writeFileSync("./public/index.html",indexTemplate.replace("<<process-env-status>>","").replace(/<<live-preview-link>>/g,""));
+  fs.writeFileSync("./public/200.html",indexTemplate.replace("<<process-env-status>>","").replace(/<<live-preview-link>>/g,""));
+  fs.writeFileSync("./public/404.html",indexTemplate.replace("<<process-env-status>>","").replace(/<<live-preview-link>>/g,""));
+}
 
 function serve() {
   let server;
