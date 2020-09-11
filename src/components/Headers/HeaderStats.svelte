a5d0eff1146ed0fa83671e4e88d37d839d213ff5
EINazare <emanuelioannazare@gmail.com>
2020-09-11 10:25:55 +0300

    refactor: change colors to orange

--- src/components/Headers/HeaderStats.svelte
+++ src/components/Headers/HeaderStats.svelte
@@ -6,7 +6,7 @@ import CardStats from "components/Cards/CardStats.svelte";
 
 
   <!-- Header -->
-  <div class="relative bg-green-600 md:pt-32 pb-32 pt-12">
+  <div class="relative bg-orange-600 md:pt-32 pb-32 pt-12">
     <div class="px-4 md:px-10 mx-auto w-full">
       <div>
         <!-- Card stats -->
