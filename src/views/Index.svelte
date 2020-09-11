bf7e5bab76b698ee64d647d5ffa1f06118b91e81
EINazare <emanuelioannazare@gmail.com>
2020-09-11 17:53:19 +0300

    refactor: replace pattern_vue with svlete one

--- src/views/Index.svelte
+++ src/views/Index.svelte
@@ -4,7 +4,7 @@
   import IndexNavbar from "components/Navbars/IndexNavbar.svelte";
   import Footer from "components/Footers/Footer.svelte";
 
-  const patternVue = "/assets/img/pattern_vue.png";
+  const patternVue = "/assets/img/pattern_svelte.png";
   const componentBtn = "/assets/img/component-btn.png";
   const componentProfileCard = "/assets/img/component-profile-card.png";
   const componentInfoCard = "/assets/img/component-info-card.png";
