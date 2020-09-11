4dd698e11b0314c42874d9ae66509aeef5acc39c
EINazare <emanuelioannazare@gmail.com>
2020-09-11 10:13:53 +0300

    feature: add CardLineChart Svelte code

--- src/components/Cards/CardLineChart.svelte
+++ src/components/Cards/CardLineChart.svelte
@@ -1,4 +1,116 @@
-<template>
+<script>
+import { onMount } from 'svelte';
+import Chart from "chart.js";
+
+
+onMount(async () => {
+  var config = {
+    type: "line",
+    data: {
+      labels: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+      ],
+      datasets: [
+        {
+          label: new Date().getFullYear(),
+          backgroundColor: "#9ae6b4",
+          borderColor: "#9ae6b4",
+          data: [65, 78, 66, 44, 56, 67, 75],
+          fill: false,
+        },
+        {
+          label: new Date().getFullYear() - 1,
+          fill: false,
+          backgroundColor: "#ed64a6",
+          borderColor: "#ed64a6",
+          data: [40, 68, 86, 74, 56, 60, 87],
+        },
+      ],
+    },
+    options: {
+      maintainAspectRatio: false,
+      responsive: true,
+      title: {
+        display: false,
+        text: "Sales Charts",
+        fontColor: "white",
+      },
+      legend: {
+        labels: {
+          fontColor: "white",
+        },
+        align: "end",
+        position: "bottom",
+      },
+      tooltips: {
+        mode: "index",
+        intersect: false,
+      },
+      hover: {
+        mode: "nearest",
+        intersect: true,
+      },
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              fontColor: "rgba(255,255,255,.7)",
+            },
+            display: true,
+            scaleLabel: {
+              display: false,
+              labelString: "Month",
+              fontColor: "white",
+            },
+            gridLines: {
+              display: false,
+              borderDash: [2],
+              borderDashOffset: [2],
+              color: "rgba(33, 37, 41, 0.3)",
+              zeroLineColor: "rgba(0, 0, 0, 0)",
+              zeroLineBorderDash: [2],
+              zeroLineBorderDashOffset: [2],
+            },
+          },
+        ],
+        yAxes: [
+          {
+            ticks: {
+              fontColor: "rgba(255,255,255,.7)",
+            },
+            display: true,
+            scaleLabel: {
+              display: false,
+              labelString: "Value",
+              fontColor: "white",
+            },
+            gridLines: {
+              borderDash: [3],
+              borderDashOffset: [3],
+              drawBorder: false,
+              color: "rgba(255, 255, 255, 0.15)",
+              zeroLineColor: "rgba(33, 37, 41, 0)",
+              zeroLineBorderDash: [2],
+              zeroLineBorderDashOffset: [2],
+            },
+          },
+        ],
+      },
+    },
+  };
+  var ctx = document.getElementById("line-chart").getContext("2d");
+  window.myLine = new Chart(ctx, config);
+});
+</script>
+
+
+
   <div
     class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-green-900"
   >
@@ -21,116 +133,3 @@
       </div>
     </div>
   </div>
-</template>
-<script>
-import Chart from "chart.js";
-
-export default {
-  mounted: function () {
-    this.$nextTick(function () {
-      var config = {
-        type: "line",
-        data: {
-          labels: [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-          ],
-          datasets: [
-            {
-              label: new Date().getFullYear(),
-              backgroundColor: "#9ae6b4",
-              borderColor: "#9ae6b4",
-              data: [65, 78, 66, 44, 56, 67, 75],
-              fill: false,
-            },
-            {
-              label: new Date().getFullYear() - 1,
-              fill: false,
-              backgroundColor: "#ed64a6",
-              borderColor: "#ed64a6",
-              data: [40, 68, 86, 74, 56, 60, 87],
-            },
-          ],
-        },
-        options: {
-          maintainAspectRatio: false,
-          responsive: true,
-          title: {
-            display: false,
-            text: "Sales Charts",
-            fontColor: "white",
-          },
-          legend: {
-            labels: {
-              fontColor: "white",
-            },
-            align: "end",
-            position: "bottom",
-          },
-          tooltips: {
-            mode: "index",
-            intersect: false,
-          },
-          hover: {
-            mode: "nearest",
-            intersect: true,
-          },
-          scales: {
-            xAxes: [
-              {
-                ticks: {
-                  fontColor: "rgba(255,255,255,.7)",
-                },
-                display: true,
-                scaleLabel: {
-                  display: false,
-                  labelString: "Month",
-                  fontColor: "white",
-                },
-                gridLines: {
-                  display: false,
-                  borderDash: [2],
-                  borderDashOffset: [2],
-                  color: "rgba(33, 37, 41, 0.3)",
-                  zeroLineColor: "rgba(0, 0, 0, 0)",
-                  zeroLineBorderDash: [2],
-                  zeroLineBorderDashOffset: [2],
-                },
-              },
-            ],
-            yAxes: [
-              {
-                ticks: {
-                  fontColor: "rgba(255,255,255,.7)",
-                },
-                display: true,
-                scaleLabel: {
-                  display: false,
-                  labelString: "Value",
-                  fontColor: "white",
-                },
-                gridLines: {
-                  borderDash: [3],
-                  borderDashOffset: [3],
-                  drawBorder: false,
-                  color: "rgba(255, 255, 255, 0.15)",
-                  zeroLineColor: "rgba(33, 37, 41, 0)",
-                  zeroLineBorderDash: [2],
-                  zeroLineBorderDashOffset: [2],
-                },
-              },
-            ],
-          },
-        },
-      };
-      var ctx = document.getElementById("line-chart").getContext("2d");
-      window.myLine = new Chart(ctx, config);
-    });
-  },
-};
-</script>
