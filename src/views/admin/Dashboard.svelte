f93e4e45f6c868186ba97f9dcea614ecee6c1243
EINazare <emanuelioannazare@gmail.com>
2020-09-11 10:15:01 +0300

    feature: add Dashboard Svelte code

--- src/views/admin/Dashboard.svelte
+++ src/views/admin/Dashboard.svelte
@@ -1,35 +1,27 @@
-<template>
+<script>
+import CardLineChart from "components/Cards/CardLineChart.svelte";
+import CardBarChart from "components/Cards/CardBarChart.svelte";
+import CardPageVisits from "components/Cards/CardPageVisits.svelte";
+import CardSocialTraffic from "components/Cards/CardSocialTraffic.svelte";
+</script>
+
+
+
   <div>
     <div class="flex flex-wrap">
       <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-        <card-line-chart />
+        <CardLineChart />
       </div>
       <div class="w-full xl:w-4/12 px-4">
-        <card-bar-chart />
+        <CardBarChart />
       </div>
     </div>
     <div class="flex flex-wrap mt-4">
       <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-        <card-page-visits />
+        <CardPageVisits />
       </div>
       <div class="w-full xl:w-4/12 px-4">
-        <card-social-traffic />
+        <CardSocialTraffic />
       </div>
     </div>
   </div>
-</template>
-<script>
-import CardLineChart from "@/components/Cards/CardLineChart.vue";
-import CardBarChart from "@/components/Cards/CardBarChart.vue";
-import CardPageVisits from "@/components/Cards/CardPageVisits.vue";
-import CardSocialTraffic from "@/components/Cards/CardSocialTraffic.vue";
-export default {
-  name: "dashboard-page",
-  components: {
-    CardLineChart,
-    CardBarChart,
-    CardPageVisits,
-    CardSocialTraffic,
-  },
-};
-</script>
