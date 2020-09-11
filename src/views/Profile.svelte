918d8f61132e26489466f19890c5c6b7f6513a53
EINazare <emanuelioannazare@gmail.com>
2020-09-11 10:26:34 +0300

    refactor: change colors to orange

--- src/views/Profile.svelte
+++ src/views/Profile.svelte
@@ -64,7 +64,7 @@ const team2 = "assets/img/team-2-800x800.jpg";
                 >
                   <div class="py-6 px-3 mt-32 sm:mt-0">
                     <button
-                      class="bg-green-500 active:bg-green-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                      class="bg-orange-500 active:bg-orange-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                       type="button"
                     >
                       Connect
@@ -135,7 +135,7 @@ const team2 = "assets/img/team-2-800x800.jpg";
                     </p>
                     <a
                       href="javascript:void(0)"
-                      class="font-normal text-green-500"
+                      class="font-normal text-orange-500"
                     >
                       Show more
                     </a>
