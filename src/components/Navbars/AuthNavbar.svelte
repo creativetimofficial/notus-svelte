90bda902f9929f6cfb1acd1df33c4eb99125b5b0
EINazare <emanuelioannazare@gmail.com>
2020-09-11 18:51:31 +0300

    refactor: try to solve github pages live preview

--- src/components/Navbars/AuthNavbar.svelte
+++ src/components/Navbars/AuthNavbar.svelte
@@ -21,7 +21,7 @@
       <a
         use:link
         class="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
-        href="/"
+        href="{process.env.PRODUCTION ? '/tailwind-webapp-svelte':''}/"
       >
         Tailwind WebApp Svelte
       </a>
