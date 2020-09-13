0606160966bb74ec20028b50b5b4cfc2b8266995
EINazare <emanuelioannazare@gmail.com>
2020-09-13 15:53:19 +0300

    fix: solve responsive issues

--- src/layouts/Auth.svelte
+++ src/layouts/Auth.svelte
@@ -15,7 +15,7 @@
 <div>
   <AuthNavbar />
   <main>
-    <section class="relative w-full h-full py-40">
+    <section class="relative w-full h-full py-40 min-h-screen">
       <div
         class="absolute top-0 w-full h-full bg-gray-900 bg-no-repeat bg-full"
         style="background-image: url({registerBg2});"
