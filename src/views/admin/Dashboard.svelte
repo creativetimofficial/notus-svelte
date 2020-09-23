65369d1dc7e8de918da310ba6851dadd03f30d24
EINazare <emanuelioannazare@gmail.com>
2020-09-23 18:27:05 +0300

    fix: solve svelte warnings

--- src/views/admin/Dashboard.svelte
+++ src/views/admin/Dashboard.svelte
@@ -4,6 +4,7 @@
   import CardBarChart from "components/Cards/CardBarChart.svelte";
   import CardPageVisits from "components/Cards/CardPageVisits.svelte";
   import CardSocialTraffic from "components/Cards/CardSocialTraffic.svelte";
+  export let location;
 </script>
 
 <div>
