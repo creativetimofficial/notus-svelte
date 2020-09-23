142b0cb65c355ad34188581d1833f02138cbd30d
EINazare <emanuelioannazare@gmail.com>
2020-09-23 18:21:36 +0300

    fix: solve svelte warnings

--- src/views/Index.svelte
+++ src/views/Index.svelte
@@ -16,6 +16,7 @@
   const login = "/assets/img/login.jpg";
   const profile = "/assets/img/profile.jpg";
   const landing = "/assets/img/landing.jpg";
+  export let location;
 </script>
 
 <IndexNavbar />
