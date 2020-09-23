41832e2144a4a8b8e1dc52137885c909be7dc95b
EINazare <emanuelioannazare@gmail.com>
2020-09-23 16:43:30 +0300

    refactor: change links and names

--- src/views/Landing.svelte
+++ src/views/Landing.svelte
@@ -38,7 +38,7 @@
               <p class="mt-4 text-lg text-gray-300">
                 This is a simple example of a Landing Page you can build using
                 Tailwind WebApp Svelte. It features multiple CSS components
-                based on the Tailwindcss design system.
+                based on the Tailwind CSS design system.
               </p>
             </div>
           </div>
