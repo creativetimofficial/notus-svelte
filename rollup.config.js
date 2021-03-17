494d9f574b78c4353b7d48caf9e1e33927f0ae7e
EINazare <emanuelioannazare@gmail.com>
2021-03-17 13:10:17 +0200

    docs: change dates and versions

--- rollup.config.js
+++ rollup.config.js
@@ -9,6 +9,7 @@ import { terser } from "rollup-plugin-terser";
 // we will be able to say
 // import Component from "components/Component.svelte";
 import alias from "@rollup/plugin-alias";
+import fs from "fs";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -21,6 +22,72 @@ const aliases = alias({
     { find: "assets", replacement: "src/assets" },
   ],
 });
+
+const indexTemplate = `<!--
+
+=========================================================
+* Notus Svelte - v1.1.0 based on Tailwind Starter Kit by Creative Tim
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/notus-svelte
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/notus-svelte/blob/master/LICENSE.md)
+
+* Tailwind Starter Kit Page: https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon.png" />
+    <link rel="stylesheet" href="/build/bundle.css" />
+    <link
+      rel="stylesheet"
+      href="/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css"
+    />
+    <link rel="stylesheet" href="/assets/styles/tailwind.css" />
+    <title>Notus Svelte</title>
+
+    <script>
+      if (process === undefined) {
+        var process = { env: {<<process-env-status>>} };
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"></script>
+    <script defer src="<<live-preview-link>>/build/bundle.js"></script>
+  </head>
+
+  <body class="text-gray-800 antialiased">
+    <noscript>
+      <strong
+        >We're sorry but notus-svelte doesn't work properly without
+        JavaScript enabled. Please enable it to continue.</strong
+      >
+    </noscript>
+    <div id="app"></div>
+  </body>
+</html>
+`
+
+if (production) {
+  fs.writeFileSync("./public/index.html",indexTemplate.replace("<<process-env-status>>","PRODUCTION: true").replace(/<<live-preview-link>>/g,"/notus-svelte"));
+  fs.writeFileSync("./public/200.html",indexTemplate.replace("<<process-env-status>>","PRODUCTION: true").replace(/<<live-preview-link>>/g,"/notus-svelte"));
+  fs.writeFileSync("./public/404.html",indexTemplate.replace("<<process-env-status>>","PRODUCTION: true").replace(/<<live-preview-link>>/g,"/notus-svelte"));
+} else {
+  fs.writeFileSync("./public/index.html",indexTemplate.replace("<<process-env-status>>","").replace(/<<live-preview-link>>/g,""));
+  fs.writeFileSync("./public/200.html",indexTemplate.replace("<<process-env-status>>","").replace(/<<live-preview-link>>/g,""));
+  fs.writeFileSync("./public/404.html",indexTemplate.replace("<<process-env-status>>","").replace(/<<live-preview-link>>/g,""));
+}
 
 function serve() {
   let server;
