96e45ebbc16857627ee65d892cbd82c3fa320263
EINazare <emanuelioannazare@gmail.com>
2021-03-18 14:00:24 +0200

    refactor: update everything

--- src/components/Cards/CardProfile.svelte
+++ src/components/Cards/CardProfile.svelte
@@ -21,54 +21,54 @@
         <div class="flex justify-center py-4 lg:pt-4 pt-8">
           <div class="mr-4 p-3 text-center">
             <span
-              class="text-xl font-bold block uppercase tracking-wide text-gray-700"
+              class="text-xl font-bold block uppercase tracking-wide text-coolGray-600"
             >
               22
             </span>
-            <span class="text-sm text-gray-500">Friends</span>
+            <span class="text-sm text-coolGray-400">Friends</span>
           </div>
           <div class="mr-4 p-3 text-center">
             <span
-              class="text-xl font-bold block uppercase tracking-wide text-gray-700"
+              class="text-xl font-bold block uppercase tracking-wide text-coolGray-600"
             >
               10
             </span>
-            <span class="text-sm text-gray-500">Photos</span>
+            <span class="text-sm text-coolGray-400">Photos</span>
           </div>
           <div class="lg:mr-4 p-3 text-center">
             <span
-              class="text-xl font-bold block uppercase tracking-wide text-gray-700"
+              class="text-xl font-bold block uppercase tracking-wide text-coolGray-600"
             >
               89
             </span>
-            <span class="text-sm text-gray-500">Comments</span>
+            <span class="text-sm text-coolGray-400">Comments</span>
           </div>
         </div>
       </div>
     </div>
     <div class="text-center mt-12">
-      <h3 class="text-xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
+      <h3 class="text-xl font-semibold leading-normal mb-2 text-coolGray-700 mb-2">
         Jenna Stones
       </h3>
       <div
-        class="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase"
+        class="text-sm leading-normal mt-0 mb-2 text-coolGray-400 font-bold uppercase"
       >
-        <i class="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i>
+        <i class="fas fa-map-marker-alt mr-2 text-lg text-coolGray-400"></i>
         Los Angeles, California
       </div>
-      <div class="mb-2 text-gray-700 mt-10">
-        <i class="fas fa-briefcase mr-2 text-lg text-gray-500"></i>
+      <div class="mb-2 text-coolGray-600 mt-10">
+        <i class="fas fa-briefcase mr-2 text-lg text-coolGray-400"></i>
         Solution Manager - Creative Tim Officer
       </div>
-      <div class="mb-2 text-gray-700">
-        <i class="fas fa-university mr-2 text-lg text-gray-500"></i>
+      <div class="mb-2 text-coolGray-600">
+        <i class="fas fa-university mr-2 text-lg text-coolGray-400"></i>
         University of Computer Science
       </div>
     </div>
-    <div class="mt-10 py-10 border-t border-gray-300 text-center">
+    <div class="mt-10 py-10 border-t border-coolGray-200 text-center">
       <div class="flex flex-wrap justify-center">
         <div class="w-full lg:w-9/12 px-4">
-          <p class="mb-4 text-lg leading-relaxed text-gray-800">
+          <p class="mb-4 text-lg leading-relaxed text-coolGray-700">
             An artist of considerable range, Jenna the name taken by
             Melbourne-raised, Brooklyn-based Nick Murphy writes, performs and
             records all of his own music, giving it a warm, intimate feel with a
