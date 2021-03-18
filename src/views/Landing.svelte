9a8b35ce049fab9bf59800a7cb067dbdc7f51f98
EINazare <emanuelioannazare@gmail.com>
2021-03-18 18:36:59 +0200

    fix: solve broken colors

--- src/views/Landing.svelte
+++ src/views/Landing.svelte
@@ -157,7 +157,7 @@
 
           <div class="w-full md:w-4/12 px-4 mr-auto ml-auto">
             <div
-              class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-red-500"
+              class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-red-400"
             >
               <img
                 alt="..."
@@ -173,7 +173,7 @@
                 >
                   <polygon
                     points="-30,95 583,95 583,65"
-                    class="text-red-500 fill-current"
+                    class="text-red-400 fill-current"
                   ></polygon>
                 </svg>
                 <h4 class="text-xl font-bold text-white">
