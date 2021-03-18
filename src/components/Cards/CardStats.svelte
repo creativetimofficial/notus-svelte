8854102b20487ac5419db263f2df1d6ddcd07f20
EINazare <emanuelioannazare@gmail.com>
2021-03-18 14:00:29 +0200

    refactor: update everything

--- src/components/Cards/CardStats.svelte
+++ src/components/Cards/CardStats.svelte
@@ -23,10 +23,10 @@
   <div class="flex-auto p-4">
     <div class="flex flex-wrap">
       <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
-        <h5 class="text-gray-500 uppercase font-bold text-xs">
+        <h5 class="text-coolGray-400 uppercase font-bold text-xs">
           {statSubtitle}
         </h5>
-        <span class="font-semibold text-xl text-gray-800">
+        <span class="font-semibold text-xl text-coolGray-700">
           {statTitle}
         </span>
       </div>
@@ -38,14 +38,14 @@
         </div>
       </div>
     </div>
-    <p class="text-sm text-gray-500 mt-4">
+    <p class="text-sm text-coolGray-400 mt-4">
       <span class="mr-2 {statPercentColor}">
         <i
           class="{statArrow === 'up' ? 'fas fa-arrow-up' : 'fas fa-arrow-down'}"
         ></i>
         {statPercent}%
       </span>
-      <span class="whitespace-no-wrap">{statDescripiron}</span>
+      <span class="whitespace-nowrap">{statDescripiron}</span>
     </p>
   </div>
 </div>
