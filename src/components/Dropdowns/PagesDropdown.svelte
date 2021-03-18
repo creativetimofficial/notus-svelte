e9e86af63ddd3fc5f732d0605de5223177756f6e
EINazare <emanuelioannazare@gmail.com>
2021-03-18 14:11:48 +0200

    refactor: start app

--- src/components/Dropdowns/PagesDropdown.svelte
+++ src/components/Dropdowns/PagesDropdown.svelte
@@ -100,7 +100,7 @@
       href="/landing"
       class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-coolGray-700"
     >
-      Lading
+      Landing
     </a>
     <a
       use:link
