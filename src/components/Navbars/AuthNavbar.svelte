0af990028588a65b2d7a1df0714595d64d249769
EINazare <emanuelioannazare@gmail.com>
2021-03-18 17:49:07 +0200

    fix: solve broken colors

--- src/components/Navbars/AuthNavbar.svelte
+++ src/components/Navbars/AuthNavbar.svelte
@@ -97,7 +97,7 @@
 
         <li class="flex items-center">
           <button
-            class="bg-white text-coolGray-700 active:bg-coolGray-new text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+            class="bg-white text-coolGray-700 active:bg-coolGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
             type="button"
           >
             <i class="fas fa-arrow-alt-circle-down"></i> Download
