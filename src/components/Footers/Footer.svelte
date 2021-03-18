c5f3db4bda07acb52537e1897a11ac73bb718f0d
EINazare <emanuelioannazare@gmail.com>
2021-03-18 18:25:31 +0200

    fix: solve broken colors

--- src/components/Footers/Footer.svelte
+++ src/components/Footers/Footer.svelte
@@ -32,13 +32,13 @@
         </h5>
         <div class="mt-6 lg:mb-0 mb-6">
           <button
-            class="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+            class="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
             type="button"
           >
             <i class="fab fa-twitter"></i>
           </button>
           <button
-            class="bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+            class="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
             type="button"
           >
             <i class="fab fa-facebook-square"></i>
