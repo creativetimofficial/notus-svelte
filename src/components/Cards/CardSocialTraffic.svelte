b00b114da2995c56f8873b44b43c436e4fe9a425
EINazare <emanuelioannazare@gmail.com>
2021-03-19 11:48:52 +0200

    refactor: change coolGray with blueGray

--- src/components/Cards/CardSocialTraffic.svelte
+++ src/components/Cards/CardSocialTraffic.svelte
@@ -4,7 +4,7 @@
   <div class="rounded-t mb-0 px-4 py-3 border-0">
     <div class="flex flex-wrap items-center">
       <div class="relative w-full px-4 max-w-full flex-grow flex-1">
-        <h3 class="font-semibold text-base text-coolGray-700">
+        <h3 class="font-semibold text-base text-blueGray-700">
           Social traffic
         </h3>
       </div>
@@ -24,17 +24,17 @@
       <thead class="thead-light">
         <tr>
           <th
-            class="px-6 bg-coolGray-50 text-coolGray-500 align-middle border border-solid border-coolGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+            class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
           >
             Referral
           </th>
           <th
-            class="px-6 bg-coolGray-50 text-coolGray-500 align-middle border border-solid border-coolGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+            class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
           >
             Visitors
           </th>
           <th
-            class="px-6 bg-coolGray-50 text-coolGray-500 align-middle border border-solid border-coolGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px"
+            class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px"
           ></th>
         </tr>
       </thead>
