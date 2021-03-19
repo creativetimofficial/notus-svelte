c984feb600f06bb690a111ba2d4dd03534540b52
EINazare <emanuelioannazare@gmail.com>
2021-03-19 11:49:12 +0200

    refactor: change coolGray with blueGray

--- src/layouts/Admin.svelte
+++ src/layouts/Admin.svelte
@@ -19,7 +19,7 @@
 
 <div>
   <Sidebar location={location}/>
-  <div class="relative md:ml-64 bg-coolGray-100">
+  <div class="relative md:ml-64 bg-blueGray-100">
     <AdminNavbar />
     <HeaderStats />
     <div class="px-4 md:px-10 mx-auto w-full -m-24">
