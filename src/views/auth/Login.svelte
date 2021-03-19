31959dc3fd2e31b49b627d4313b47342a7d9ec54
EINazare <emanuelioannazare@gmail.com>
2021-03-19 11:49:18 +0200

    refactor: change coolGray with blueGray

--- src/views/auth/Login.svelte
+++ src/views/auth/Login.svelte
@@ -11,40 +11,40 @@
   <div class="flex content-center items-center justify-center h-full">
     <div class="w-full lg:w-4/12 px-4">
       <div
-        class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-coolGray-200 border-0"
+        class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0"
       >
         <div class="rounded-t mb-0 px-6 py-6">
           <div class="text-center mb-3">
-            <h6 class="text-coolGray-500 text-sm font-bold">
+            <h6 class="text-blueGray-500 text-sm font-bold">
               Sign in with
             </h6>
           </div>
           <div class="btn-wrapper text-center">
             <button
-              class="bg-white active:bg-coolGray-50 text-coolGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+              class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
               type="button"
             >
               <img alt="..." class="w-5 mr-1" src="{github}" />
               Github
             </button>
             <button
-              class="bg-white active:bg-coolGray-50 text-coolGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+              class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
               type="button"
             >
               <img alt="..." class="w-5 mr-1" src="{google}" />
               Google
             </button>
           </div>
-          <hr class="mt-6 border-b-1 border-coolGray-300" />
+          <hr class="mt-6 border-b-1 border-blueGray-300" />
         </div>
         <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
-          <div class="text-coolGray-400 text-center mb-3 font-bold">
+          <div class="text-blueGray-400 text-center mb-3 font-bold">
             <small>Or sign in with credentials</small>
           </div>
           <form>
             <div class="relative w-full mb-3">
               <label
-                class="block uppercase text-coolGray-600 text-xs font-bold mb-2"
+                class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                 for="grid-email"
               >
                 Email
@@ -52,14 +52,14 @@
               <input
                 id="grid-email"
                 type="email"
-                class="border-0 px-3 py-3 placeholder-coolGray-300 text-coolGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                 placeholder="Email"
               />
             </div>
 
             <div class="relative w-full mb-3">
               <label
-                class="block uppercase text-coolGray-600 text-xs font-bold mb-2"
+                class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                 for="grid-password"
               >
                 Password
@@ -67,7 +67,7 @@
               <input
                 id="grid-password"
                 type="password"
-                class="border-0 px-3 py-3 placeholder-coolGray-300 text-coolGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                 placeholder="Password"
               />
             </div>
@@ -76,9 +76,9 @@
                 <input
                   id="customCheckLogin"
                   type="checkbox"
-                  class="form-checkbox border-0 rounded text-coolGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
+                  class="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
                 />
-                <span class="ml-2 text-sm font-semibold text-coolGray-600">
+                <span class="ml-2 text-sm font-semibold text-blueGray-600">
                   Remember me
                 </span>
               </label>
@@ -86,7 +86,7 @@
 
             <div class="text-center mt-6">
               <button
-                class="bg-coolGray-800 text-white active:bg-coolGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                class="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                 type="button"
               >
                 Sign In
@@ -97,12 +97,12 @@
       </div>
       <div class="flex flex-wrap mt-6 relative">
         <div class="w-1/2">
-          <a href="#pablo" on:click={(e) => e.preventDefault()} class="text-coolGray-200">
+          <a href="#pablo" on:click={(e) => e.preventDefault()} class="text-blueGray-200">
             <small>Forgot password?</small>
           </a>
         </div>
         <div class="w-1/2 text-right">
-          <a use:link href="/auth/register" class="text-coolGray-200">
+          <a use:link href="/auth/register" class="text-blueGray-200">
             <small>Create new account</small>
           </a>
         </div>
