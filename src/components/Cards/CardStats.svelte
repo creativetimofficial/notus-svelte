413fb2f6493c26e3b5743755bb8f4794af196ec3
EINazare <emanuelioannazare@gmail.com>
2021-03-19 11:48:53 +0200

    refactor: change coolGray with blueGray

--- src/components/Cards/CardStats.svelte
+++ src/components/Cards/CardStats.svelte
@@ -23,10 +23,10 @@
   <div class="flex-auto p-4">
     <div class="flex flex-wrap">
       <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
-        <h5 class="text-coolGray-400 uppercase font-bold text-xs">
+        <h5 class="text-blueGray-400 uppercase font-bold text-xs">
           {statSubtitle}
         </h5>
-        <span class="font-semibold text-xl text-coolGray-700">
+        <span class="font-semibold text-xl text-blueGray-700">
           {statTitle}
         </span>
       </div>
@@ -38,7 +38,7 @@
         </div>
       </div>
     </div>
-    <p class="text-sm text-coolGray-400 mt-4">
+    <p class="text-sm text-blueGray-400 mt-4">
       <span class="mr-2 {statPercentColor}">
         <i
           class="{statArrow === 'up' ? 'fas fa-arrow-up' : 'fas fa-arrow-down'}"
