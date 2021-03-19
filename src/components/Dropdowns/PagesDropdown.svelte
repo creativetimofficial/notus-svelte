f54918335b12c9d88bd1316bdb6a8eca2e3900c0
EINazare <emanuelioannazare@gmail.com>
2021-03-19 11:48:59 +0200

    refactor: change coolGray with blueGray

--- src/components/Dropdowns/PagesDropdown.svelte
+++ src/components/Dropdowns/PagesDropdown.svelte
@@ -25,7 +25,7 @@
 
 <div>
   <a
-    class="lg:text-white lg:hover:text-coolGray-200 text-coolGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+    class="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
     href="#pablo"
     bind:this="{btnDropdownRef}"
     on:click="{toggleDropdown}"
@@ -37,75 +37,75 @@
     class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48 {dropdownPopoverShow ? 'block':'hidden'}"
   >
     <span
-      class="text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-coolGray-400"
+      class="text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400"
     >
       Admin Layout
     </span>
     <a
       use:link
       href="/admin/dashboard"
-      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-coolGray-700"
+      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
     >
       Dashboard
     </a>
     <a
       use:link
       href="/admin/settings"
-      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-coolGray-700"
+      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
     >
       Settings
     </a>
     <a
       use:link
       href="/admin/tables"
-      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-coolGray-700"
+      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
     >
       Tables
     </a>
     <a
       use:link
       href="/admin/maps"
-      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-coolGray-700"
+      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
     >
       Maps
     </a>
-    <div class="h-0 mx-4 my-2 border border-solid border-coolGray-100" />
+    <div class="h-0 mx-4 my-2 border border-solid border-blueGray-100" />
     <span
-      class="text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-coolGray-400"
+      class="text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400"
     >
       Auth Layout
     </span>
     <a
       use:link
       href="/auth/login"
-      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-coolGray-700"
+      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
     >
       Login
     </a>
     <a
       use:link
       href="/auth/register"
-      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-coolGray-700"
+      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
     >
       Register
     </a>
-    <div class="h-0 mx-4 my-2 border border-solid border-coolGray-100" />
+    <div class="h-0 mx-4 my-2 border border-solid border-blueGray-100" />
     <span
-      class="text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-coolGray-400"
+      class="text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400"
     >
       No Layout
     </span>
     <a
       use:link
       href="/landing"
-      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-coolGray-700"
+      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
     >
       Landing
     </a>
     <a
       use:link
       href="/profile"
-      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-coolGray-700"
+      class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
     >
       Profile
     </a>
